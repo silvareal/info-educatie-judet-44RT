69ad951a34840e2427ebaa8cc25f54217373d016
Constanda Valentin Marian <constandavalentin@gmail.com>
2017-05-27 16:50:02 +0300

    Added style and functionality to the main page of AdminCP

---
+++ src/public/components/Admin/Main Component/Admin.jsx
@@ -0,0 +1,75 @@
+import React, {Component} from 'react';
+import {Card, CardMedia, CardHeader} from 'material-ui';
+import {Link} from 'react-router';
+
+import ActionAccountCircle from 'material-ui/svg-icons/action/account-circle';
+import ActionAccountBox from 'material-ui/svg-icons/action/account-box';
+import ImageCollections from 'material-ui/svg-icons/image/collections';
+import ActionAnnouncement from 'material-ui/svg-icons/action/announcement';
+
+class AdminPage extends Component {
+
+    render() {
+
+        const styles = {
+
+            cardMedia: {
+                textAlign: "center"
+            }
+        };
+
+        return (
+            <div>
+                <div className="top-bar-spacing"/>
+                <Card className="container-admin-cp" style={{boxShadow: "none"}}>
+                    <div className="mobile-header-admin-cp">
+                        <CardHeader title=
+                                        {
+                                            <div className="user-header">
+                                                Welcome {this.props.firstName ? this.props.firstName : this.props.userName}!
+                                            </div>
+                                        }
+                                    avatar={this.props.profilePictureLink ? this.props.profilePictureLink :
+                                        <ActionAccountCircle/>}
+                        />
+                    </div>
+                    <div className="icons-container-admin-cp">
+                        <CardMedia style={styles.cardMedia}>
+                            <Link to={`/admin/${this.props.adminId}/users`}>
+                                <div className="icons-content-admin-cp">
+                                    <ActionAccountBox/>
+                                </div>
+                            </Link>
+                            <Link to={`/admin/${this.props.adminId}/users`}>
+                                <div className="action-header">Users management</div>
+                            </Link>
+                        </CardMedia>
+                        <CardMedia style={styles.cardMedia}>
+                            <Link to={`/admin/${this.props.adminId}/collections`}>
+                                <div className="icons-content-admin-cp">
+                                    <ImageCollections/>
+                                </div>
+                            </Link>
+                            <Link to={`/admin/${this.props.adminId}/collections`}>
+                                <div className="action-header">Collections management</div>
+                            </Link>
+                        </CardMedia>
+                        <CardMedia style={styles.cardMedia}>
+                            <Link to={`/admin/${this.props.adminId}/news`}>
+                                <div className="icons-content-admin-cp">
+                                    <ActionAnnouncement/>
+                                </div>
+                            </Link>
+                            <Link to={`/admin/${this.props.adminId}/news`}>
+                                <div className="action-header">News management</div>
+                            </Link>
+                        </CardMedia>
+                    </div>
+                </Card>
+            </
+                div >
+        )
+    }
+}
+
+export default AdminPage;
--- src/public/containers/Admin/Main Component/AdminView.jsx
+++ src/public/containers/Admin/Main Component/AdminView.jsx
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 
-import AdminPage from '../../../components/Admin/Main Component/AdminPage.jsx';
+import AdminPage from '../../../components/Admin/Main Component/Admin.jsx';
 import Auth from '../../../modules/Auth.js';
-import NotAuthorizedPage from '../../Error/NotAuthorizedView.jsx';
+import NotAuthorizedView from '../../Error/NotAuthorizedView.jsx';
 
 class AdminView extends Component {
     constructor(props) {
@@ -10,13 +10,15 @@ class AdminView extends Component {
 
         this.state = {
             isAdmin: false,
-            users: {},
-            message: ''
+            message: '',
+            userName: '',
+            firstName:'',
+            profilePictureLink: '',
+            adminId:''
         };
     }
 
-    //Check if the user that requests this page is an admin or not
-    componentDidMount() {
+    adminAuth = () => {
         const xhr = new XMLHttpRequest();
         xhr.open('get', '/admin/adminAuthentication');
         xhr.setRequestHeader('Authorization', `bearer ${Auth.getToken()}`);
@@ -31,6 +33,31 @@ class AdminView extends Component {
             }
         });
         xhr.send();
+    };
+
+    getUser = () => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("get", "/comment/getUserCredentials");
+        xhr.setRequestHeader('Authorization', `bearer ${Auth.getToken()}`);
+        xhr.responseType = 'json';
+        xhr.addEventListener('load', () => {
+            if (xhr.status === 200) {
+                this.setState({
+                    userName: xhr.response.userName,
+                    firstName: xhr.response.firstName,
+                    profilePictureLink: xhr.response.profilePictureLink,
+                    adminId: xhr.response.userId
+                });
+            }
+        });
+
+        xhr.send();
+    };
+
+    //Check if the user that requests this page is an admin or not
+    componentDidMount() {
+        this.adminAuth();
+        this.getUser();
     }
 
     render() {
@@ -39,9 +66,13 @@ class AdminView extends Component {
             <div>
                 {this.state.isAdmin ?
                     <AdminPage
+                        adminId={this.state.adminId}
+                        userName={this.state.userName}
+                        firstName={this.state.firstName}
+                        profilePictureLink={this.state.profilePictureLink}
                         userId={this.props.params._id}
                         message={this.state.message}
-                    /> : <NotAuthorizedPage/>}
+                    /> : <NotAuthorizedView/>}
             </div>
         )
     }
